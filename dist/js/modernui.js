7dfcbfd60e500ff2c619c2340ef6c1b12bcbe2a6
Elijah Gummer <elijahgummer5@gmail.com>
2024-05-08 11:40:43 +1000

    added some js code

--- dist/js/modernui.js
+++ dist/js/modernui.js
@@ -20,3 +20,17 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+// NAVBAR RESPONSIVE CODE
+
+const navbar = document.querySelector(".navbar");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = () => {
+  navbar.classList.add("show");
+  menuBtn.classList.add("hide");
+};
+cancelBtn.onclick = () => {
+  navbar.classList.remove("show");
+  menuBtn.classList.remove("hide");
+};
